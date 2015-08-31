4ec735fece1afd2d556aa75c8d31d4c793db40f5
Clint Schaeffer <neclint@yahoo.com>
2015-08-31 21:38:42 +0000

    Added listtoArray method finally

--- chapter4.js
+++ chapter4.js
@@ -1,7 +1,7 @@
 // List using references
 function arrayToList(arr){
   	var list = {value: arr[arr.length-1], rest: null};
-	for(i=arr.length-2;i>=0;i--){
+	for(var i=arr.length-2;i>=0;i--){
 	  list = {value: arr[i], rest: list};
 	};
 	return list;
@@ -9,11 +9,20 @@ function arrayToList(arr){
 
 function listToArray(tlist){
 	var arr = [];
-	for (var prop in tlist) {
-		if (! prop === 'value') continue;
-		arr.push(tlist[prop]);
+	for (var node = tlist; node; node = node.rest)	{
+		arr.push(node.value);
 	}
-	return arr;
+	return arr;	
 }
+
 function prepend(){}
 function nth(){}
+
+//console.log(arrayToList([10, 20]));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(listToArray(arrayToList([10, 20, 30])));
+// → [10, 20, 30]
+console.log(prepend(10, prepend(20, null)));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(nth(arrayToList([10, 20, 30]), 1));
+// → 20
